a066c610ca7dc74c226bf7aa4ebc101f188db40f
intae <jjojjo141995@gmail.com>
2023-05-06 11:31:12 +0900

    refactor 코드 정리

---
+++ src/components/room/Room.styled.ts
@@ -0,0 +1,182 @@
+import styled from 'styled-components';
+import { highlight } from '../../style';
+import { TOP_MENU_HEIGHT } from '../../style/constants';
+
+export const RoomContent = styled.div`
+  width: 100%;
+  height: calc(100% - ${TOP_MENU_HEIGHT}px);
+  display: flex;
+`;
+
+export const MemberList = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 240px;
+  padding: 16px;
+
+  background-color: ${({ theme }) => theme.color.primary800};
+
+  .member-item:nth-child(1) {
+    color: ${({ theme }) => theme.color.primary100};
+    font-weight: bold;
+  }
+  .member-item {
+    color: ${({ theme }) => theme.color.grey100};
+    margin-bottom: 16px;
+    path {
+      stroke: #000;
+    }
+    .ant-btn {
+      margin-left: 4px;
+    }
+    .screen-share-button {
+      animation: ${highlight} 2s 1s infinite linear alternate;
+    }
+  }
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+`;
+
+export const VideoContainer = styled.div<{ isVisible: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  width: ${({ isVisible }) => (isVisible ? '70%' : '0px')};
+  height: 100%;
+  transition: 0.3s;
+  background-color: ${({ theme }) => theme.color.primary400};
+
+  video {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const ChatContainer = styled.div`
+  width: 100%;
+  min-width: 30%;
+  position: relative;
+  .ant-btn-dangerous {
+    position: absolute;
+    left: calc(50% - 16px);
+    bottom: 110px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    path {
+      stroke: #ff7875;
+      fill: #ff7875;
+    }
+    animation: ${highlight} 2s 1s infinite linear alternate;
+  }
+`;
+
+export const ChatList = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  width: 100%;
+  height: calc(100% - 100px);
+  overflow: overlay;
+  padding: 20px 40px 0 40px;
+  .chat-bubble {
+    max-width: 100%;
+  }
+  .peer-chat {
+    margin-bottom: 8px;
+    align-self: flex-start;
+    .chat-date {
+      display: flex;
+      justify-content: flex-start;
+    }
+  }
+  .my-chat {
+    margin-bottom: 8px;
+
+    align-self: flex-end;
+    .chat-date {
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+  .chat-name,
+  .chat-date {
+    span {
+      color: ${({ theme }) => theme.color.primary200};
+      font-size: 12px;
+    }
+  }
+`;
+
+export const ChatForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 20px;
+  right: 20px;
+  bottom: 20px;
+
+  height: 80px;
+
+  border: 1px solid ${({ theme }) => theme.color.primary400};
+  border-radius: 8px;
+
+  textarea {
+    width: 100%;
+    height: 100%;
+
+    border: 0;
+    border-top-right-radius: 8px;
+    border-top-left-radius: 8px;
+
+    padding: 8px;
+
+    resize: none;
+    box-sizing: border-box;
+    border-bottom: 1px solid ${({ theme }) => theme.color.primary400};
+  }
+  textarea::placeholder {
+  }
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    border: 0;
+    svg {
+      width: 24px;
+      height: 24px;
+    }
+  }
+  button:disabled {
+    cursor: default;
+    svg {
+      path {
+        stroke: ${({ theme }) => theme.color.primary400};
+      }
+    }
+  }
+
+  .form-header {
+    height: 40px;
+  }
+
+  .form-footer {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    height: 100%;
+    background-color: ${({ theme }) => theme.color.grey800};
+    border-bottom-right-radius: 8px;
+    border-bottom-left-radius: 8px;
+    button {
+      background-color: ${({ theme }) => theme.color.grey800};
+    }
+  }
+`;
--- src/components/room/Room.tsx
+++ src/components/room/Room.tsx
@@ -3,7 +3,6 @@ import dayjs from 'dayjs';
 import { nanoid } from 'nanoid';
 import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
 import { AppServiceImpl } from '../../service/app/AppServiceImpl';
 import { videoManager } from '../../service/media/VideoManager';
 import { StorageService } from '../../service/storage/StorageService';
@@ -11,14 +10,21 @@ import { utils } from '../../service/utils/Utils';
 import { chatActions } from '../../store/features/chatSlice';
 import { roomActions } from '../../store/features/roomSlice';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { highlight } from '../../style';
-import { TOP_MENU_HEIGHT } from '../../style/constants';
 import ChatBubble from '../chat/ChatBubble';
 import TopMenuContainer from '../container/TopMenuContainer';
 import SvgArrowDown from '../icons/ArrowDown';
 import SvgScreenShareOn from '../icons/ScreenShareOn';
 import SvgSend from '../icons/Send';
 import SvgSpeakerOn from '../icons/SpeakerOn';
+import {
+  ChatContainer,
+  ChatForm,
+  ChatList,
+  ContentContainer,
+  MemberList,
+  RoomContent,
+  VideoContainer,
+} from './Room.styled';
 import UsernameModal from './UsernameModal';
 
 const storage = StorageService.getInstance();
@@ -26,7 +32,7 @@ const storage = StorageService.getInstance();
 const Room = () => {
   const chatScrollViewElement = useRef<HTMLDivElement>(null);
   const chatListElement = useRef<HTMLDivElement>(null);
-  const [open, setOpen] = useState(false);
+  const [usernameModalVisible, setUsernameModalVisible] = useState(false);
   const [selectedClientId, setSelectedClientId] = useState('');
   const app = useRef(AppServiceImpl.getInstance()).current;
   const { roomName } = useParams<{
@@ -71,7 +77,7 @@ const Room = () => {
       dispatch(roomActions.setRoomName(roomName));
       storage.setItem('roomName', roomName);
     } else {
-      setOpen(true);
+      setUsernameModalVisible(true);
     }
   }, [myName, roomName]);
 
@@ -210,7 +216,7 @@ const Room = () => {
                   onChange={(e) => setMessage(e.target.value)}
                   name="message"
                   style={{ height: 40 }}
-                  placeholder={roomName?.split('_')[0] + '에 메시지 보내기'}
+                  placeholder={roomName?.split('_')[0] + ' 에 메시지 보내기'}
                 />
               </div>
 
@@ -223,195 +229,16 @@ const Room = () => {
           </ChatContainer>
         </ContentContainer>
         <UsernameModal
-          open={open}
+          open={usernameModalVisible}
           onSubmit={(username) => {
             dispatch(roomActions.setUsername({ username }));
             storage.setItem('username', username);
-            setOpen(false);
+            setUsernameModalVisible(false);
           }}
         />
       </RoomContent>
     </>
   );
 };
-
-const RoomContent = styled.div`
-  width: 100%;
-  height: calc(100% - ${TOP_MENU_HEIGHT}px);
-  display: flex;
-`;
-
-const MemberList = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 240px;
-  padding: 16px;
-
-  background-color: ${({ theme }) => theme.color.primary800};
-
-  .member-item:nth-child(1) {
-    color: ${({ theme }) => theme.color.primary100};
-    font-weight: bold;
-  }
-  .member-item {
-    color: ${({ theme }) => theme.color.grey100};
-    margin-bottom: 16px;
-    path {
-      stroke: #000;
-    }
-    .ant-btn {
-      margin-left: 4px;
-    }
-    .screen-share-button {
-      animation: ${highlight} 2s 1s infinite linear alternate;
-    }
-  }
-`;
-
-const ContentContainer = styled.div`
-  display: flex;
-  width: 100%;
-  position: relative;
-`;
-
-const VideoContainer = styled.div<{ isVisible: boolean }>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-shrink: 0;
-  width: ${({ isVisible }) => (isVisible ? '70%' : '0px')};
-  height: 100%;
-  transition: 0.3s;
-  background-color: ${({ theme }) => theme.color.primary400};
-
-  video {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-const ChatContainer = styled.div`
-  width: 100%;
-  min-width: 30%;
-  position: relative;
-  .ant-btn-dangerous {
-    position: absolute;
-    left: calc(50% - 16px);
-    bottom: 110px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    path {
-      stroke: #ff7875;
-      fill: #ff7875;
-    }
-    animation: ${highlight} 2s 1s infinite linear alternate;
-  }
-`;
-
-const ChatList = styled.div`
-  display: flex;
-  box-sizing: border-box;
-  flex-direction: column;
-  width: 100%;
-  height: calc(100% - 100px);
-  overflow: overlay;
-  padding: 20px 40px 0 40px;
-  .chat-bubble {
-    max-width: 100%;
-  }
-  .peer-chat {
-    margin-bottom: 8px;
-    align-self: flex-start;
-    .chat-date {
-      display: flex;
-      justify-content: flex-start;
-    }
-  }
-  .my-chat {
-    margin-bottom: 8px;
-
-    align-self: flex-end;
-    .chat-date {
-      display: flex;
-      justify-content: flex-end;
-    }
-  }
-  .chat-name,
-  .chat-date {
-    span {
-      color: ${({ theme }) => theme.color.primary200};
-      font-size: 12px;
-    }
-  }
-`;
-
-const ChatForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  left: 20px;
-  right: 20px;
-  bottom: 20px;
-
-  height: 80px;
-
-  border: 1px solid ${({ theme }) => theme.color.primary400};
-  border-radius: 8px;
-
-  textarea {
-    width: 100%;
-    height: 100%;
-
-    border: 0;
-    border-top-right-radius: 8px;
-    border-top-left-radius: 8px;
-
-    padding: 8px;
-
-    resize: none;
-    box-sizing: border-box;
-    border-bottom: 1px solid ${({ theme }) => theme.color.primary400};
-  }
-  textarea::placeholder {
-  }
-
-  button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    border: 0;
-    svg {
-      width: 24px;
-      height: 24px;
-    }
-  }
-  button:disabled {
-    cursor: default;
-    svg {
-      path {
-        stroke: ${({ theme }) => theme.color.primary400};
-      }
-    }
-  }
-
-  .form-header {
-    height: 40px;
-  }
-
-  .form-footer {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    height: 100%;
-    background-color: ${({ theme }) => theme.color.grey800};
-    border-bottom-right-radius: 8px;
-    border-bottom-left-radius: 8px;
-    button {
-      background-color: ${({ theme }) => theme.color.grey800};
-    }
-  }
-`;
 
 export default Room;
