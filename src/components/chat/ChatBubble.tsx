878269fc59ab3b93512760879f10f96749ed877e
intae <jjojjo141995@gmail.com>
2023-05-22 23:55:47 +0900

    챗버블 유저명 별 리본 색 랜덤

--- src/components/chat/ChatBubble.tsx
+++ src/components/chat/ChatBubble.tsx
@@ -1,6 +1,7 @@
 import { Badge, Card } from 'antd';
+import color from 'color';
+import stc from 'string-to-color';
 import styled from 'styled-components';
-import { theme } from '../../style/theme';
 
 interface ChatBubbleProps {
   date: string;
@@ -11,12 +12,21 @@ interface ChatBubbleProps {
 
 const ChatBubble = ({ message, date, username, isMyChat }: ChatBubbleProps) => {
   const padding = isMyChat ? '10px 80px 10px 20px' : '10px 20px 10px 80px';
+  const sum = (arr: number[]) =>
+    arr.reduce((prev, curr) => {
+      return prev + curr;
+    }, 0);
+  const getColorObj = (colorCode: string) => color(colorCode) as any;
+  const isBlack = () =>
+    sum(getColorObj(stc(username)).color as number[]) / 765 > 0.6;
   return (
-    <div
+    <ChatBubbleContainer
+      $isBlack={isBlack()}
       className={isMyChat ? 'chat-bubble  my-chat' : 'chat-bubble peer-chat'}
     >
       <Badge.Ribbon
-        color={isMyChat ? theme.color.primary200 : theme.color.primary400}
+        style={isMyChat ? { display: 'none' } : {}}
+        color={stc(username)}
         placement={isMyChat ? 'end' : 'start'}
         text={username}
       >
@@ -27,9 +37,15 @@ const ChatBubble = ({ message, date, username, isMyChat }: ChatBubbleProps) => {
       <div className={'chat-date'}>
         <span>{date}</span>
       </div>
-    </div>
+    </ChatBubbleContainer>
   );
 };
+
+const ChatBubbleContainer = styled.div<{ $isBlack: boolean }>`
+  .ant-ribbon-text {
+    color: ${({ $isBlack }) => ($isBlack ? '#333' : '#eee')};
+  }
+`;
 
 const StyledCard = styled(Card)`
   p {
