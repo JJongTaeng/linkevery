c8ebb5f5def5acdad5f3d6094d70dac53bb1ce48
조인태 <63296903+JJongTaeng@users.noreply.github.com>
2024-04-17 21:47:24 +0900

    [fix] init gtm prod 환경에서만 되도록 수정

--- src/initGTM.ts
+++ src/initGTM.ts
@@ -3,8 +3,7 @@ import TagManger from 'react-gtm-module';
 const tagManagerArgs = {
   gtmId: 'GTM-MR4B7C37',
 };
-
-if (!(import.meta.env.NODE_ENV === 'development')) {
+if (import.meta.env.PROD) {
   TagManger.initialize(tagManagerArgs);
 } else {
   window.dataLayer = [];
