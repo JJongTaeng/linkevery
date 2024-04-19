18b68ea6cbba3b2725c62db8eabaea65ced252a7
조인태 <63296903+JJongTaeng@users.noreply.github.com>
2024-04-19 21:45:16 +0900

    fix: log

--- src/service/event/EventManager.ts
+++ src/service/event/EventManager.ts
@@ -1,9 +1,12 @@
 import EventEmitter from 'events';
-import { EVENT_NAME, EventType } from '../../constants/eventType';
+import { EVENT_NAME, EventType } from 'constants/eventType';
+import { storage } from 'service/storage/StorageService.ts';
 
 export class EventManager extends EventEmitter {
   send(protocol: EventType) {
+    const clientId = storage.getItem('clientId');
+
     console.debug('%c[send] ', 'color:green;font-weight:bold;', protocol);
-    this.emit(EVENT_NAME, protocol);
+    this.emit(EVENT_NAME, { ...protocol, from: clientId });
   }
 }
